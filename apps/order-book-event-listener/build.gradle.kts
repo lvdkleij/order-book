d3e2978a032f9da101faf38a8c1c35ac2f7ca52e
Lucas van der Kleij <kleijvander@gmail.com>
2025-08-30 09:32:10 +0200

    init orderbookeventlistener app

---
+++ apps/order-book-event-listener/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+}
+
